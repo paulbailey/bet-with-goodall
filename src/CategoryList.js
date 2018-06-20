99cc8196969b5059c59cff7235576cb6f66a8f7e
Paul Bailey <paulbailey@users.noreply.github.com>
2018-06-20 10:15:35 +0100

    Add better navbar

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -33,12 +33,18 @@ class CategoryList extends Component {
     })
 
     return (
-      <div>
-        <ul className='nav'>
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+          <a className='navbar-brand' href ='#'>Bet With Goodall</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
           {cats}
         </ul>
         {categories}
-      </div>
+        </div>
+        </nav>
     )
   }
 }
