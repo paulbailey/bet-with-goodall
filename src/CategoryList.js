86a1078b006ee11cf24b53fa0cf8a66cbd6f2373
Paul Bailey <paulbailey@users.noreply.github.com>
2018-06-20 10:18:41 +0100

    Add anchor text

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -34,7 +34,7 @@ class CategoryList extends Component {
 
     return (
         <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-          <a className='navbar-brand' href ='#'>Bet With Goodall</a>
+          <a className='navbar-brand' href ='#home'>Bet With Goodall</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
